e7342120f7c1153368035ce2ee82c710a2fb5b15
relex <m9barry@gmail.com>
2025-09-25 13:07:12 +0000

    Basic PostCard created

---
+++ lib/helper/demo_values.dart
@@ -0,0 +1,10 @@
+class DemoValues {
+  static final String userImage = "assets/images/profile.jpg";
+  static final String postImage = "assets/images/post.jpg";
+  static final String userName = "Relex Brown";
+  static final String userEmail = "m9barry@gmail.com";
+  static final String postTime = "30 April, 2019";
+  static final String postTitle = "Maple Trees";
+  static final String postSummary =
+      "A comparative study of maple leaves and other plants";
+}
--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/view/pages/widgets/post_card.dart';
 
 class HomePage extends StatelessWidget {
   // ignore: use_super_parameters
@@ -8,7 +9,12 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Leaf")),
-      body: Center(child: Text("Hello world!")),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (BuildContext context, int index) {
+          return PostCard();
+        },
+      ),
     );
   }
 }
